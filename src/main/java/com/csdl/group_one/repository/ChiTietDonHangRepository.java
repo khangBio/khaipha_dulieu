d072ae0ff3c17e6e57572182596faf6fa29afd80
khangtn3894 <khangtn3894@gmail.com>
2025-12-04 21:01:07 +0700

    clear source

--- src/main/java/com/csdl/group_one/repository/ChiTietDonHangRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csdl.group_one.repository;
-
-import com.csdl.group_one.model.ChiTietDonHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChiTietDonHangRepository extends JpaRepository<ChiTietDonHang, Integer> {
-}
--- src/main/java/com/csdl/group_one/repository/DonHangRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csdl.group_one.repository;
-
-import com.csdl.group_one.model.DonHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonHangRepository extends JpaRepository<DonHang, Integer> {
-}
